581db18c9752d7ef6916275c876485c027cf3c24
Simon <simon.j.goring@gmail.com>
2015-03-19 14:25:45 -0500

    Changing the `ui` and `server` scripts to support a multi-column checkbox system. Output now generated as a list.

--- server.R
+++ server.R
@@ -14,7 +14,7 @@ shinyServer(function(input, output, session) {
   #  The 'flag' starts out as 'x'
   flag <<- 'x'
   randVal <- formatC(round(runif(1,0,1000000),0), width = 6, format = "d", flag = "0")
-  dateTime <- gsub(' ', '_', as.character(Sys.time()))
+  dateTime <- gsub('[ |:]', '_', as.character(Sys.time()))
   #initialState <- reactiveValues(A = length(messages))
   
   values <- reactive({
@@ -31,32 +31,38 @@ shinyServer(function(input, output, session) {
     }
     isolate({  
       
-      new <- evals[1,]
-      new[1,1] <- isolate(values())
-      new[1,1 + as.numeric(isolate(input$checkGroup))] <- 1
+      new <- isolate(list(values(), input$jobType, input$jobPay, 
+                                     input$jobQuals, input$interDiscip,
+                                     input$certainty))
       new$flag <- flag
 
       flag <<- 'a'
-      evals <<- rbind(evals, new)
+      evals <<- list(evals, new)
       saveRDS(evals, file=paste0('data/evals',randVal, dateTime,'.RDS'))
-      
-      updateCheckboxGroupInput(session, 'checkGroup', 
-                               choices=list("Interdisciplinary" = 1, 
-                                            "Job Ad" = 2,
-                                            "Seasonal" = 3,
-                                            "Internship" = 4,
-                                            "Tenure Track" = 5,
-                                            "Postdoc" = 6,
-                                            "Grad student" = 7,
-                                            "Undergraduate" = 8,
-                                            "Scholarship" = 9,
-                                            "Unpaid" = 10,
-                                            "Small Stipend" = 11,
-                                            "Some Uncertainty (Reviewer)" = 12), 
-                               selected=NULL)
+
+      updateCheckboxGroupInput(session, "jobType",
+                         choices = list("Job Ad" = 1, "Grad Position" = 2,
+                                        "Internship" = 3, "Seasonal" = 4,
+                                        "Tenure Track" = 5, "Postdoc" = 6),
+                         selected = NULL)
+      updateCheckboxGroupInput(session, "jobPay",
+                         choices = list("Salary/Hourly" = 1, "Scholarship" = 2,
+                                        "Stipend" = 3, "Unpaid" = 4),
+                         selected = NULL)    
+      updateCheckboxGroupInput(session, "jobQuals", 
+                         choices = list("Faculty" = 1, "Postdoc+" = 2,
+                                        "Grad Student" = 3, "Undergrad" = 4),
+                         selected = NULL)
+      updateCheckboxGroupInput(session, "interDiscip", 
+                         choices = list("Interdisciplinary" = 1),
+                         selected = NULL)
+      updateCheckboxGroupInput(session, "certainty", 
+                        choices = list("Something I'm not sure of" = 1),
+                        selected = NULL)
       
       HTML(paste0('Msg:', values(), ' - ', subjects[[values()]],'<br/>',
-                          messages[[values()]]))
+                          gsub('[inter|multi|trans]disciplin*', '<b><u>interdisciplinary</u></b>', 
+                               messages[[values()]])))
     })
   })
 
--- ui.R
+++ ui.R
@@ -1,24 +1,30 @@
 shinyUI(pageWithSidebar(
   headerPanel('Download Example'),
   sidebarPanel(
-    
-    checkboxGroupInput("checkGroup", 
-                       label = h3("Checkbox group"), 
-                       choices = list("Interdisciplinary" = 1, 
-                                      "Job Ad" = 2,
-                                      "Seasonal" = 3,
-                                      "Internship" = 4,
-                                      "Tenure Track" = 5,
-                                      "Postdoc" = 6,
-                                      "Grad student" = 7,
-                                      "Undergraduate" = 8,
-                                      "Scholarship" = 9,
-                                      "Unpaid" = 10,
-                                      "Small Stipend" = 11,
-                                      "Some Uncertainty (Reviewer)" = 12)),
-    
-    actionButton(inputId='click', label='Confirm')
-  ),
+    fixedRow(
+      column(4, checkboxGroupInput("jobType", 
+                         label = h3("Job Type"), 
+                         choices = list("Job Ad" = 1, "Grad Position" = 2,
+                                        "Internship" = 3, "Seasonal" = 4,
+                                        "Tenure Track" = 5, "Postdoc" = 6)),
+             checkboxGroupInput("jobPay", 
+                                label = h3("Job Pay"), 
+                                choices = list("Salary/Hourly" = 1, "Scholarship" = 2,
+                                               "Stipend" = 3, "Unpaid" = 4))),
+      
+      column(4, checkboxGroupInput("jobQuals", 
+                                   label = h3("Job Qualifications"), 
+                                   choices = list("Faculty" = 1, "Postdoc+" = 2,
+                                                  "Grad Student" = 3, "Undergrad" = 4)),
+             checkboxGroupInput("interDiscip", 
+                                label = h3("Other"), 
+                                choices = list("Interdisciplinary" = 1)),
+             checkboxGroupInput("certainty", 
+                                label = h3("Are You Sure?"), 
+                                choices = list("Something I'm not sure of" = 1))
+             )),
+              
+    actionButton(inputId='click', label='Confirm')),
   mainPanel(
     htmlOutput('table')
   )
