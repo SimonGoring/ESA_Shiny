ddf895bbe2e4e5a41b58ef42ecc8fb92500787fe
Simon <simon.j.goring@gmail.com>
2015-06-14 10:32:59 -0500

    Saves & outputs the `not_jobs` file.

--- R/merge_RDS.R
+++ R/merge_RDS.R
@@ -87,3 +87,7 @@ parse_files <- function(x){
 
 
 all_runs <- na.omit(do.call(rbind.data.frame, lapply(all_RDS, parse_files)))
+
+not_job <- all_runs$msg[which(all_runs$jt_na == TRUE)]
+
+saveRDS(object = not_job, file = 'data/not_jobs.RDS')
