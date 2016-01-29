f7dbab809f95811d1e144856ee76682cb19b7c96
Simon <simon.j.goring@gmail.com>
2016-01-28 20:31:39 -0800

    A large number of changes:
    Paper:
    * Changed unit of analysis from weeks to months and saved output of `all_melt` as an RDS file.
    
    `load_models`:
    * cleaning message classes.  Removed all messages that had no classification effort (messages were viewed but not classified)
    * Cleaned up the space around the `full_class` assignments.
    * Added two extra class assignments, "Other job" and "Non-job".  This splits the "Other" category in an effort to help reduce confusion with "Other" and cleans up the boolean logic for the classes.
    * removed the assignments to particular column names.  The fixed number assignment was no lnger changing the names, and the issue has been resolved elsewhere anyway.
    * Class balancing in the `randomForest` model is now dynamic rather than fixed.
    * Ultimately these changes reduces the model's confusion rate to ~16%.
    
    `data_annotation`:
    * Added a new markdown file to provide a place to add lots of plots.
    
    `jobs_eda`:
    * The basis for the Bayesian modeling.
    * Changed "Class" to "variable", which is the column name used in creating `all_melt`.
    
    `randomForest_testing`:
    * I wanted to see how sample number affects the classification.  Looks like we're getting into an assymptote, but futher work will help show this more clearly.  Saved the output.

---
+++ R/randomForest_testing.R
@@ -0,0 +1,83 @@
+#  Testing the effect of hand classification:
+#  This file loads all RDS files following the system file pattern as defined
+#  in the shiny scripts (server.R and ui.R).
+
+#  During this process we build the term matrix for the top 99% of terms
+#  all run in the `load_term.R` file.
+
+#  Using the hand coded Ecolog posts we then build RF models for the individual 
+#  messages.  This is partly to facilitate hand coding, since mesages with a 
+#  high confusion rate are given a high likelihood of being selected for hand
+#  coding in the Shiny app.
+
+curr_RDS <- list.files('data', pattern = '*evals[0-9][0-9]*', full.names = TRUE)
+old_RDS  <- readRDS('data/RDS_list.RDS')
+
+source('R/merge_RDS.R')
+source('R/load_term.R')
+
+dtm.s99 <- as.data.frame(as.matrix(dtm.s99))
+
+# To build the model:
+library(randomForest)
+
+all_runs <- readRDS(file = 'data/responses.RDS')
+
+#  Clean out messages that were skipped (had no classification effort)
+all_runs <- all_runs[rowSums(all_runs[,-1]) > 0 & !(rowSums(all_runs[,-1]) > 0 & all_runs$uncer),]
+
+#  In these groups there is potential for overlap, but we assign a class:
+tenure_track <- na.omit(data.frame(tt.job = factor(all_runs$jt_tt==1),
+                                   dtm.s99[all_runs$msg,]>0))
+
+postdoc      <- na.omit(data.frame(postdo = factor(all_runs$jt_pd==1),
+                                   dtm.s99[all_runs$msg,]>0))
+
+grad_student <- na.omit(data.frame(gradst = factor(all_runs$jt_gp==1),
+                                   dtm.s99[all_runs$msg,]>0))
+
+interdiscip  <- na.omit(data.frame(interd = factor(all_runs$inter==1),
+                                   dtm.s99[all_runs$msg,]>0))
+
+full_class <- rep(NA, nrow(all_runs))
+
+## Classifying the messages into a single factor level based on some boolean choices.
+full_class[all_runs$jt_tt & !(all_runs$jt_pd & all_runs$jt_gp & all_runs$inter)] <- "TT"
+full_class[all_runs$jt_tt & all_runs$inter & !(all_runs$jt_pd & all_runs$jt_gp)] <- "TT_Int"
+
+full_class[all_runs$jt_pd & !(all_runs$jt_tt & all_runs$jt_gp & all_runs$inter)] <- "PD"
+full_class[all_runs$jt_pd & all_runs$inter & !(all_runs$jt_tt & all_runs$jt_gp)] <- "PD_Int"
+
+full_class[all_runs$jt_gp & !(all_runs$jt_tt & all_runs$jt_pd & all_runs$inter)] <- "GR"
+full_class[all_runs$jt_gp & all_runs$inter & !(all_runs$jt_tt & all_runs$jt_pd)] <- "GR_Int"
+
+full_class[!(all_runs$jt_tt | all_runs$jt_pd | all_runs$jt_gp) & 
+             (all_runs$jt_ad | all_runs$jt_se | all_runs$jt_in)] <- "Other job"
+
+full_class[all_runs$jt_na | is.na(full_class)] <- "Non-job"
+
+full_df      <- na.omit(data.frame(fm = full_class,
+                                   dtm.s99[all_runs$msg,]>0))
+
+dtm.pred <- data.frame(dtm.s99>0)
+
+conf_drop <- rep(NA, 10)
+  
+for(i in 1:10){
+  # The messages are in classified order.  We have the actual message dates, but I'm more interested
+  #  in how well the messages classify as a function of training set size:
+  
+  test_df <- full_df[1:(nrow(full_df)/10 * i),]
+  
+  freq <- min(table(test_df$fm))
+  
+  test.rf   <- randomForest(fm     ~ ., data = test_df, 
+                            sampsize = rep(freq,length(unique(test_df[,1]))), # balance the classes.
+                            mtry = 127) # mtry comes from tuneRF.
+  conf_drop[i] <- sum((test.rf$confusion[,9] * rowSums(test.rf$confusion[,-9])))/sum(test.rf$confusion[,-9])
+}
+
+conf_drop <- data.frame(samples = (nrow(full_df)/10 * i),
+                        confusion = conf_drop)
+
+saveRDS(conf_drop, 'data/conf_drop.RDS')
--- model/jobs_eda.R
+++ model/jobs_eda.R
@@ -5,35 +5,35 @@ all_dat = readRDS('data/all_melt.RDS')
 dat = all_dat[,c(1,2,3,4)]
 # dat = data.frame(dat, date=all_dat$year+all_dat$week_dec)
 
-tots = aggregate(value ~ date + class, dat, sum)
+tots = aggregate(value ~ date + variable, dat, sum)
 
-int = dat[dat$int==TRUE,c('class', 'date', 'value')]
-dat_org = merge(int, tots, by = c('date', 'class'))
+int = dat[dat$int==TRUE,c('variable', 'date', 'value')]
+dat_org = merge(int, tots, by = c('date', 'variable'))
 colnames(dat_org)[3:4] = c('int', 'total')
 dat_org = data.frame(dat_org, nint = dat_org$total - dat_org$int)
 
 # totals
-ggplot(dat_org) + geom_line(data=dat_org, aes(x=date, y=total)) + facet_grid(class~.)
+ggplot(dat_org) + geom_line(data=dat_org, aes(x=date, y=total)) + facet_grid(variable~.)
 # int only
-ggplot(dat_org) + geom_line(data=dat_org, aes(x=date, y=int)) + facet_grid(class~.)
+ggplot(dat_org) + geom_line(data=dat_org, aes(x=date, y=int)) + facet_grid(variable~.)
 # nint only
-ggplot(dat_org) + geom_line(data=dat_org, aes(x=date, y=total-int)) + facet_grid(class~.)
+ggplot(dat_org) + geom_line(data=dat_org, aes(x=date, y=total-int)) + facet_grid(variable~.)
 # prop nint to total
-ggplot(dat_org) + geom_line(data=dat_org, aes(x=date, y=(total-int)/total)) + facet_grid(class~.)
+ggplot(dat_org) + geom_line(data=dat_org, aes(x=date, y=(total-int)/total)) + facet_grid(variable~.)
 # prop int to total
-ggplot(dat_org) + geom_line(data=dat_org, aes(x=date, y=(int)/total)) + facet_grid(class~.)
+ggplot(dat_org) + geom_line(data=dat_org, aes(x=date, y=(int)/total)) + facet_grid(variable~.)
 
 # prop nint to total
-ggplot(dat_org) + geom_line(data=dat_org, aes(x=date, y=(total-int)/total, colour=class)) #+ facet_grid(class~.)
+ggplot(dat_org) + geom_line(data=dat_org, aes(x=date, y=(total-int)/total, colour=variable)) #+ facet_grid(variable~.)
 # prop int to total
-ggplot(dat_org) + geom_line(data=dat_org, aes(x=date, y=int/total, colour=class)) #+ facet_grid(class~.)
+ggplot(dat_org) + geom_line(data=dat_org, aes(x=date, y=int/total, colour=variable)) #+ facet_grid(variable~.)
 
-levels(dat_org$class) <- c('GR', 'PD', 'TT')
+levels(dat_org$variable) <- c('GR', 'PD', 'TT')
 
 #####################################################################################################################
 ## GR
 #####################################################################################################################
-gr_dat = dat_org[which(dat_org$class == 'GR'),]
+gr_dat = dat_org[which(dat_org$variable == 'GR'),]
 gr_dat = gr_dat[order(gr_dat$date),]
 
 gr_dat$nint_p = gr_dat$nint/gr_dat$total
@@ -65,7 +65,7 @@ sarima.for(gr_dat$nint_p, 40, 1, 1, 0, 0, 1, 1, 12)
 #####################################################################################################################
 ## PD
 #####################################################################################################################
-pd_dat = dat_org[which(dat_org$class == 'PD'),]
+pd_dat = dat_org[which(dat_org$variable == 'PD'),]
 pd_dat = pd_dat[order(pd_dat$date),]
 
 pd_dat$nint_p = pd_dat$nint/pd_dat$total
@@ -78,7 +78,7 @@ lag.plot(pd_dat$nint_p, lags=12)
 #####################################################################################################################
 ## TT
 #####################################################################################################################
-tt_dat = dat_org[which(dat_org$class == 'TT'),]
+tt_dat = dat_org[which(dat_org$variable == 'TT'),]
 tt_dat = tt_dat[order(tt_dat$date),]
 
 tt_dat$nint_p = tt_dat$nint/tt_dat$total
